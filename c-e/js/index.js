7e4ab347d3c42c6e4b425a74781d290e0d6bcfae
alexter72 <tereshinaleks@gmail.com>
2020-07-23 12:34:48 +0300

    add cat-energy

---
+++ c-e/js/index.js
@@ -0,0 +1,17 @@
+// работа с навигационным окном 
+
+let navMain = document.querySelector(".main-nav");
+let navToggle = document.querySelector(".main-nav--toggle");
+
+
+navMain.classList.remove("main-nav--nojs");
+
+navToggle.addEventListener("click", function () {
+	if (navMain.classList.contains("main-nav--closed")) {
+		navMain.classList.remove("main-nav--closed");
+		navMain.classList.add("main-nav--opened");
+	} else {
+		navMain.classList.remove("main-nav--opened");
+		navMain.classList.add("main-nav--closed");
+	}
+});
