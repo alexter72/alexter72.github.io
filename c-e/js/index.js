81cb3905c8b724bd5031086cf2148abd1b5e9288
alexter72 <tereshinaleks@gmail.com>
2020-11-26 20:33:01 +0200

    c-e_button_up

--- c-e/js/index.js
+++ c-e/js/index.js
@@ -15,3 +15,30 @@ navToggle.addEventListener("click", function () {
 		navMain.classList.add("main-nav--closed");
 	}
 });
+
+// перемещение в начало страницы
+
+$(document).ready(function () {
+	// hide #back-top first
+	$('.button-return').hide();
+
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 200) {
+				$('.button-return').fadeIn();
+			} else {
+				$('.button-return').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('.button-return').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+});
